87d10cd798f68c0fd63efacd4aadb5fe29c422fa
Roky Balboa <rokyunive@hotmail.it>
2016-02-10 11:57:47 +0100

    Classe Person

---
+++ project/fFormations/fFormations/Persona.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fFormations
+{
+    public class Person
+    {
+        //private int ID;
+        private double coordX;
+        private double coordY;
+        private double angle;
+
+        public int ID { get; private set; }
+
+        public double CoordX { get; private set}
+
+        public Person(int id, double coordX, double coordY, double angle) {
+            this.ID = id;
+            this.coordX = coordX;
+            this.coordY = coordY;
+            this.angle = angle;
+        }
+
+        public static bool operator ==(Person a, Person b)
+        {
+            return a.ID == b.ID;
+        }
+        public static bool operator !=(Person a, Person b)
+        {
+            return a.ID != b.ID;
+        }
+
+        public override bool Equals(object obj) {
+            Person temp = obj as Person;
+            return temp == null ? false : temp == this;
+        }
+    }
+}
